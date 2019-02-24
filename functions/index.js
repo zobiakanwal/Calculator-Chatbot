1d734ba93a00ba5b0f03cd1a492108e36f189afc
unknown <xobiaaijaz@gmail.com>
2019-02-24 02:31:44 -0800

    final commit

--- functions/index.js
+++ functions/index.js
@@ -1,46 +1,56 @@
 const functions = require('firebase-functions');
+
+
+
 exports.calculatorbot = functions.https.onRequest((request, response) => {
-console.log("request.body.result.parameters: ", request.body.result.parameters);
-let params = request.body.result.parameters; 
-var sum = Number(params.number1) + Number(params.number2);
-var sub = Number(params.number1) - Number(params.number2);
-var mul = Number(params.number1) * Number(params.number2);
-var div = Number(params.number1) / Number(params.number2);
-var mod = Number(params.number1) % Number(params.number2);
+    
+    console.log("request.body.result.parameters: ", request.body.result.parameters);
+    
+    let params = request.body.result.parameters;
+
+    console.log("params: ", params)
+
+    var sum = Number(params.number1) + Number(params.number2);
+    var sub = Number(params.number1) - Number(params.number2);
+    var mul = Number(params.number1) * Number(params.number2);
+    var div = Number(params.number1) / Number(params.number2);
+    var mod = Number(params.number1) % Number(params.number2);
+
+
     if (params.operator === "addition") {
         response.send({
-                speech:
-                `Here is your answer: ${sum}`             
-    });
-}
+            speech:
+                `Here is your answer: ${sum}`
+        });
+    }
     else if (params.operator === "subtraction") {
-                response.send({
-                        speech:
-                        `Here is your answer: ${sub}`            
-            });
-        }
+        response.send({
+            speech:
+                `Here is your answer: ${sub}`
+        });
+    }
     else if (params.operator === "multiplication") {
-               response.send({
-                       speech:
-                       `Here is your answer: ${mul}`            
-            });
-       }
+        response.send({
+            speech:
+                `Here is your answer: ${mul}`
+        });
+    }
     else if (params.operator === "division") {
         response.send({
-                speech:
-                `Here is your answer: ${div}`            
-            });
-       }
+            speech:
+                `Here is your answer: ${div}`
+        });
+    }
     else if (params.operator === "modulus") {
         response.send({
-                speech:
-                `Here is your answer: ${mod}`            
-            });
-       }
-       else {
+            speech:
+                `Here is your answer: ${mod}`
+        });
+    }
+    else {
         response.send({
-                speech:
-                `Invalid operator! I can perform addition, multiplication, division, modulus and subtraction`            
-            });
-       }
+            speech:
+                `Invalid operator! I can perform addition, multiplication, division, modulus and subtraction till now.`
+        });
+    }
 });
